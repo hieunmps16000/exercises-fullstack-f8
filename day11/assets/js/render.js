395a674b51bb4c03a084024f01c5b319a0018af1
Nguyen Minh Hieu <lotous12a4@gmail.com>
2026-09-16 14:30:04 +0700

    Update UI

--- day11/assets/js/render.js
+++ day11/assets/js/render.js
@@ -31,7 +31,7 @@ const billHtmls = cart.items
 const totalBill = `
         <ul>
             <li class="leading-[2]">Tổng tiền hàng: <span>${toVND(cart.subtotal)} VNĐ</span></li>
-            <li class="leading-[2]">Giảm giá: <span class="px-[4px] rounded-[4px] bg-[#e000ef] text-white">${cart._discountRate * 100}%</span> (-${toVND(cart.subtotal * cart._discountRate)}) VNĐ</li>
+            <li class="leading-[2]">Giảm giá: <span class="px-[4px] rounded-[4px] bg-[#e000ef] text-white">${cart._discountRate * 100}%</span> (-${toVND(cart.subtotal * cart._discountRate)} VNĐ)</li>
             <li class="mt-3 font-bold text-white text-[20px]">Tổng thanh toán: ${toVND(cart.totalPrice)} VNĐ</li>
         </ul>
     `;
